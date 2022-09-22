3ef502920d3b361f372e300c239a12455b3d37c0
Ivan C <cordovaivan8@gmail.com>
2022-09-22 14:44:40 -0700

    added a submit handler

--- script.js
+++ script.js
@@ -3,8 +3,25 @@ var cityInputEl = document.querySelector('#city');
 var APIKey = "6da0b49999367300f70db98e448e05ee";
 var city;
 
+var formSubmitHandler = function (event) {
+    event.preventDefault();
 
-fetch ("https://api.openweathermap.org/data/2.5/weather?q=phoenix&appid=6da0b49999367300f70db98e448e05ee")
+    var Data = cityInputEl.value.trim();
+
+    if(data) {
+        WeatherData(city);
+
+        weatherCard.textContent = '';
+        cityInputEl.value = '';
+    } else {
+        alert('Search a City');
+    }
+};
+
+var WeatherData = function(City) {
+    var apiWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + City + "&appid=" + APIKey;
+
+    fetch (apiWeather)
 
 .then((response) => {
     if(response.ok) {
@@ -14,24 +31,42 @@ fetch ("https://api.openweathermap.org/data/2.5/weather?q=phoenix&appid=6da0b499
     }
 })
 
-.then(data => {
-    console.log(data);
-    displayWeather(data)
-})
+// .then(data => {
+//     console.log(data);
+//     displayWeather(data)
+// })
 
-.catch((error) => console.error("FETCH ERROR:", error));
+// .catch((error) => console.error("FETCH ERROR:", error));
+// }
 
-var formSubmitHandler = function (event) {
-    event.preventDefault();
+// fetch (apiWeather)
 
-    var Weather = cityInputEl.ariaValueMax.trim();
+// .then((response) => {
+//     if(response.ok) {
+//         return response.json();
+//     } else {
+//         throw new Error("NETWORK RESPONSE ERROR");
+//     }
+// })
 
-    if(city) {
-        weatherCitylist(city);
+// .then(data => {
+//     console.log(data);
+//     displayWeather(data)
+// })
 
-        document.getElementByIda('Weather-Info').textContent = '';
-        cityInputEl.value = '';
-    } else {
-        alert('City not Found');
-    }
-};
+// .catch((error) => console.error("FETCH ERROR:", error));
+
+// var formSubmitHandler = function (event) {
+//     event.preventDefault();
+
+//     var Weather = cityInputEl.ariaValueMax.trim();
+
+//     if(city) {
+//         weatherCitylist(city);
+
+//         document.getElementByIda('Weather-Info').textContent = '';
+//         cityInputEl.value = '';
+//     } else {
+//         alert('City not Found');
+//     }
+// };
