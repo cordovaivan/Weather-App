2a90fef534e50bff9e01c70fca0cb7e2b61fca28
Ivan C <cordovaivan8@gmail.com>
2022-09-26 13:01:03 -0700

    parsed data to append to html

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-var weatherCard = document.getElementsByClassName('card-text');
+var weatherCard = document.getElementById("weatherData");
 var cityInputEl = document.getElementById('City');
-var searchBtn = document.getElementById('search')
+var searchBtn = document.getElementById('search');
+var temp = document.getElementById("temp");
+var wind = document.getElementById("wind");
+var humidity = document.getElementById("humidity");
+var city = document.getElementById("cityName");
 var APIKey = "6da0b49999367300f70db98e448e05ee";
 
 
@@ -21,7 +25,7 @@ var formSubmitHandler = function (event) {
 
 var WeatherData = function(City) {
 
-    var apiWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInputEl.value + "&appid=6da0b49999367300f70db98e448e05ee";
+    var apiWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInputEl.value + "&appid=6da0b49999367300f70db98e448e05ee&units=imperial";
 
     fetch(apiWeather)
 
@@ -35,6 +39,12 @@ var WeatherData = function(City) {
 
     .then(data => {
         console.log(data);
+        console.log(data.main.temp);
+        console.log(data.main.humidity);
+        console.log(data.wind.speed);
+        city.textContent = data.name;
+        weatherCard.textContent = data.main.temp;
+        wind.appendChild(weatherCard).textContent = data.wind.speed;
     })
 
     .catch((error) => console.error("FETCH ERROR:", error));
